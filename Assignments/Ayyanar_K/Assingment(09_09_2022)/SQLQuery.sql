b741475e3d651a31d4d652aaef1e55e9fbd2c77e
ayyanar19 <113657501+ayyanar19@users.noreply.github.com>
2022-09-30 08:45:45 +0530

    Add files via upload

---
+++ Assignments/Ayyanar_K/Assingment(09_09_2022)/SQLQuery.sql
@@ -0,0 +1,25 @@
+USE [User_database]
+GO
+
+/****** Object:  Table [dbo].[IBM_Students_Database]    Script Date: 9/30/2022 7:56:01 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[IBM_Students_Database](
+	[SI.NO] [bigint] IDENTITY(1,1) NOT NULL,
+	[USER_NAME] [varchar](50) NOT NULL,
+	[MAIL_ID] [varchar](50) NOT NULL,
+	[ROLL_NUMBER] [bigint] NOT NULL,
+	[PASSWORD] [varchar](50) NOT NULL,
+	[PHONE_NUMBER] [bigint] NULL,
+ CONSTRAINT [PK_User_Database] PRIMARY KEY CLUSTERED 
+(
+	[SI.NO] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
